5b7edf5e54e531768366eea0da63d9fb67b24173
Azim Momin <azimmomin123@gmail.com>
2017-02-11 17:27:21 -0600

    Added comments to code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,4 @@
+//Retreiving infromation from the data friend file
 var friend = require("../data/friend");
 
 
@@ -21,23 +22,37 @@ module.exports = function(app) {
     friend.push(newSurveyFriend);
     //console.log(friend.length);
 
+    //identifying the user array who most recently entered the survey so we can find a friend for them
     var x = friend.length-1;
+    //Giving total difference a value of 0 prior to the calcultion
     var totalDifference = 0;
+    //Givinge lowestScore a ridiculious number so that we can slowly find out which friend
+    //in our list has the lowest possible difference
     var lowestScore = 1000;
     var closestFriend;
+    //For loop that goes through each friend while doing another for loop through each
+    //score and finding the difference for the most recent user entered survey.
     for (var i = 0; i < friend.length - 1; i++) {
       for (var j = 0; j < 10; j++) {
+        //Calculating difference between each respective score
         var diff = friend[i].scores[j] - friend[x].scores[j];
+        //Getting the absolute value of the difference
         diff = Math.abs(diff);
+        //Calculating the total difference for all numbers
         totalDifference = totalDifference + diff;
       }
+      //Checking to see which one has the lowest score
       if(totalDifference < lowestScore){
         lowestScore = totalDifference;
         //console.log("lowestScore " + lowestScore);
         //console.log("friend: " + friend[i].name);
+
+        //assigning the closestFriend the value of the object with the lowest score
         closestFriend = friend[i];
       }
       //console.log("totalDifference " + totalDifference);
+      //We want to redefine the totalDifference to 0 after the loop is finished going
+      //through each score
       totalDifference = 0;
     }
     //console.log("Final Friend: " + JSON.stringify(closestFriend));
